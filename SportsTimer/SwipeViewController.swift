f3145639458170ddf08980dd8beeb0a5a9d6cb99
crudnicki35 <cmrudnicki@bellsouth.net>
2016-06-28 22:27:50 -0400

    Added phone functionality
    1. iPhone: Now functions similarly to the watch except that to change
    the score, the user must swipe rather than press a button.
    2. Apple Watch: (Same as last commit) Has a “Start game” button that
    leads directly to a
    timer and a scoreboard. It also buzzes and shows the user the player
    with the most goals when the timer ends.

--- SportsTimer/SwipeViewController.swift
+++ SportsTimer/SwipeViewController.swift
@@ -9,45 +9,78 @@
 import UIKit
 
 class SwipeViewController: UIViewController {
+
+    //The timer that the user can see
+    @IBOutlet weak var timerLabel: UILabel!
+    //A variable to keep track of the time in the background
+    var count = 20
+    //A NSTimer variable
+    var timer: NSTimer = NSTimer()
+    //Label for Player 1's score
+    @IBOutlet weak var player1Score: UILabel!
+    //Variable for Player 1's score
+    var score1 = 0
+    //Label for Player 2's score
+    @IBOutlet weak var player2Score: UILabel!
+    //Variable for Player 2's score
+    var score2 = 0
     
-    @IBOutlet weak var swipeLabel: UILabel!
-
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //Setting the original text of the timerLabel
+        timerLabel.text = String(count)
+        
+        //Setting the original text for each of the player's scores
+        player1Score.text = String(score1)
+        player2Score.text = String(score2)
+        
+        //Making the timer count down from the preset number
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(SwipeViewController.UpdateTimer), userInfo: nil, repeats: true)
+        
+        //Naming variables for both the left and right swipe gestures
         let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(SwipeViewController.handleSwipes(_:)))
         let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(SwipeViewController.handleSwipes(_:)))
+        
+        //Setting the direction for the leftwipe and rightSwipe variables
         leftSwipe.direction = .Left
         rightSwipe.direction = .Right
+        
+        //Add the left and right swiping gestures to the view
         view.addGestureRecognizer(leftSwipe)
         view.addGestureRecognizer(rightSwipe)
+        
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
+    //This function handles each swipe and tells the program what to do after each swipe
     func handleSwipes(sender: UISwipeGestureRecognizer) {
+        
+        //For a left swipe
         if sender.direction == .Left {
-            self.view.backgroundColor = UIColor.blueColor()
+            score1 += 1
+            player1Score.text = String(score1)
         }
         
+        //For a right swipe
         if sender.direction == .Right {
-            self.view.backgroundColor = UIColor.redColor()
+            score2 += 1
+            player2Score.text = String(score2)
         }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //This function updates the timerLabel so it always shows the correct time
+    func UpdateTimer() {
+        
+        count -= 1
+        timerLabel.text = String(count)
+        
     }
-    */
-
 }

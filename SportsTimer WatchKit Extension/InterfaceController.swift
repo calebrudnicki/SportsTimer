3d37b061d967dee8636437a2949264df66ecd192
crudnicki35 <cmrudnicki@bellsouth.net>
2016-07-05 15:46:10 -0400

    Swipe functionality clarification
    Functionality added: the swipe functionality now shuts off at the end
    of the timer
    Also, there was some restructuring in the code

--- SportsTimer WatchKit Extension/InterfaceController.swift
+++ SportsTimer WatchKit Extension/InterfaceController.swift
@@ -10,34 +10,54 @@ import WatchKit
 import Foundation
 
 class InterfaceController: WKInterfaceController {
-
-    @IBOutlet var timer: WKInterfaceTimer!                      //The timer that the user can see
-    var backingTimer: NSTimer?                                  //The timer that triggers the ending sound
-    @IBOutlet var player1Score: WKInterfaceButton!              //Player 1's score displayed to the user
-    @IBOutlet var player2Score: WKInterfaceButton!              //Player 2's score displayed to the user
-    var score1 = 0                                              //Variable to keep track of Player 1's score
-    var score2 = 0                                              //Variable to keep track of Player 2's score
     
+//MARK: Outlets
+
+    //The timer that the user can see
+    @IBOutlet var timer: WKInterfaceTimer!
+    //Player 1's score displayed to the user
+    @IBOutlet var player1Score: WKInterfaceButton!
+    //Player 2's score displayed to the user
+    @IBOutlet var player2Score: WKInterfaceButton!
+    
+//MARK: Variables
+    
+    //The timer that works behind the scenes
+    var backingTimer: NSTimer?
+    var score1 = 0
+    var score2 = 0
+    
+//MARK: Setting Default Values and Starting a New Game
+
     override func awakeWithContext(context: AnyObject?) {
+        
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
         newGame()
+        
+        ////Setting the original text for each of the player's scores
         player1Score.setTitle(String(score1))
         player2Score.setTitle(String(score2))
     }
+    
+//MARK: Activating and Deactivating
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
+        
         super.willActivate()
+        
     }
 
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
+        
         super.didDeactivate()
+        
     }
     
+//MARK: Starting a New Game
+    
     @IBAction func newGame() {
+        
         //This code starts the timer that the user can see
         let countdown: NSTimeInterval = 20
         let date = NSDate(timeIntervalSinceNow: countdown)
@@ -48,9 +68,13 @@ class InterfaceController: WKInterfaceController {
         backingTimer = NSTimer.scheduledTimerWithTimeInterval(countdown, target: self, selector: #selector(InterfaceController.timesUp), userInfo: nil, repeats: false)
     }
     
+//MARK: End of the Time
+    
     //This function runs when the timer runs out. First it notifies the user that the game is over then it display the result and disables the buttons
     func timesUp() {
+        
         WKInterfaceDevice().playHaptic(.Failure)
+        
         if score1 > score2 {
             player1Score.setTitle("W")
             player1Score.setBackgroundColor(UIColor.greenColor())
@@ -67,19 +91,27 @@ class InterfaceController: WKInterfaceController {
             player2Score.setTitle("T")
             player2Score.setBackgroundColor(UIColor.blueColor())
         }
+        
         player1Score.setEnabled(false)
         player2Score.setEnabled(false)
+        
     }
+    
+//MARK: Button Handlers
     
     //This function adds a goal to Player 1's score
     @IBAction func goalButton1() {
+        
         score1 = score1 + 1
         player1Score.setTitle(String(score1))
+        
     }
     
     //This functions adds a goal to Player 2's score
     @IBAction func goalButton2() {
+        
         score2 = score2 + 1
         player2Score.setTitle(String(score2))
+        
     }
 }
--- SportsTimer/SwipeViewController.swift
+++ SportsTimer/SwipeViewController.swift
@@ -7,80 +7,119 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class SwipeViewController: UIViewController {
+    
+//MARK: Outlets
 
-    //The timer that the user can see
     @IBOutlet weak var timerLabel: UILabel!
-    //A variable to keep track of the time in the background
-    var count = 20
-    //A NSTimer variable
-    var timer: NSTimer = NSTimer()
-    //Label for Player 1's score
     @IBOutlet weak var player1Score: UILabel!
-    //Variable for Player 1's score
-    var score1 = 0
-    //Label for Player 2's score
     @IBOutlet weak var player2Score: UILabel!
-    //Variable for Player 2's score
+    
+    
+//MARK: Variables
+
+    let leftSwipe = UISwipeGestureRecognizer()
+    let rightSwipe = UISwipeGestureRecognizer()
+    var timer: NSTimer = NSTimer()
+    var count = 20
+    var score1 = 0
     var score2 = 0
     
+    
+//MARK: viewDidLoad()
+    
+    //This function starts a new game and also sets all labels to their preset values
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
-        //Setting the original text of the timerLabel
+        newGame()
         timerLabel.text = String(count)
-        
-        //Setting the original text for each of the player's scores
         player1Score.text = String(score1)
         player2Score.text = String(score2)
-        
-        //Making the timer count down from the preset number
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(SwipeViewController.UpdateTimer), userInfo: nil, repeats: true)
-        
-        //Naming variables for both the left and right swipe gestures
-        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(SwipeViewController.handleSwipes(_:)))
-        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(SwipeViewController.handleSwipes(_:)))
-        
-        //Setting the direction for the leftwipe and rightSwipe variables
-        leftSwipe.direction = .Left
-        rightSwipe.direction = .Right
-        
-        //Add the left and right swiping gestures to the view
-        view.addGestureRecognizer(leftSwipe)
-        view.addGestureRecognizer(rightSwipe)
-        
     }
+    
+    
+//MARK: didReceiveMemoryWarning()
 
     override func didReceiveMemoryWarning() {
-        
         super.didReceiveMemoryWarning()
+    }
+    
+    
+//MARK: Starting a New Game
+    
+    //This function starts a new game by starting the timer and calling iniating the swipe functionality
+    func newGame() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(SwipeViewController.updateTimer), userInfo: nil, repeats: true)
+        initiateOrEndSwipes()
+    }
+    
+    
+//MARK: Timer Functions
+    
+    //This function updates the timerLabel so it always shows the correct time and checks to see if the times has run out
+    func updateTimer() {
+        count -= 1
+        print(count)
+        if count == -1 {
+            timer.invalidate()
+            timesUp()
+        } else {
+            timerLabel.text = String(count)
+        }
         
     }
+    
+    //This function runs when the timer runs out. It disables the swipe functionality, plays a notification sound and displays the winner and loser to the user
+    func timesUp() {
+        initiateOrEndSwipes()
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        if score1 > score2 {
+            player1Score.text = "W"
+            player1Score.textColor = UIColor.greenColor()
+            player2Score.text = "L"
+            player2Score.textColor = UIColor.redColor()
+        } else if score2 > score1 {
+            player2Score.text = "W"
+            player2Score.textColor = UIColor.greenColor()
+            player1Score.text = "L"
+            player1Score.textColor = UIColor.redColor()
+        } else {
+            player1Score.text = "T"
+            player1Score.textColor = UIColor.blueColor()
+            player2Score.text = "T"
+            player2Score.textColor = UIColor.blueColor()
+        }
+    }
+   
+    
+//MARK: Swipe Functions
     
     //This function handles each swipe and tells the program what to do after each swipe
     func handleSwipes(sender: UISwipeGestureRecognizer) {
-        
-        //For a left swipe
-        if sender.direction == .Left {
+        if sender.direction == .Right {
             score1 += 1
             player1Score.text = String(score1)
         }
-        
-        //For a right swipe
-        if sender.direction == .Right {
+        if sender.direction == .Left {
             score2 += 1
             player2Score.text = String(score2)
         }
-        
     }
     
-    //This function updates the timerLabel so it always shows the correct time
-    func UpdateTimer() {
-        
-        count -= 1
-        timerLabel.text = String(count)
-        
+    //This function creates the swipe functionalty, yet if the timer is over, it will disable the swipe functionality
+    func initiateOrEndSwipes() {
+        leftSwipe.addTarget(self, action: #selector(SwipeViewController.handleSwipes(_:)))
+        rightSwipe.addTarget(self, action: #selector(SwipeViewController.handleSwipes(_:)))
+        self.view.addGestureRecognizer(leftSwipe)
+        self.view.addGestureRecognizer(rightSwipe)
+        leftSwipe.direction = .Left
+        rightSwipe.direction = .Right
+        if count < 0 {
+            leftSwipe.enabled = false
+            rightSwipe.enabled = false
+        }
     }
+    
 }
